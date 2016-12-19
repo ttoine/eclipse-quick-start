90a41d999f38bc4e80092c3495ae3eebc3873c3c
Antoine THOMAS <antoine@eclipse.org>
2016-12-19 15:30:57 +0100

    first mockup version

---
+++ Eclipse-first-start_files/resources/story.js
@@ -0,0 +1,211 @@
+var story = {
+ "pages": [
+  {
+   "image": "first-start.png",
+   "width": 1329,
+   "links": [
+    {
+     "rect": [
+      748,
+      441,
+      814,
+      467
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      440,
+      441,
+      512,
+      467
+     ],
+     "page": 9
+    }
+   ],
+   "title": "first-start",
+   "height": 748
+  },
+  {
+   "image": "authorize.png",
+   "width": 1329,
+   "links": [
+    {
+     "rect": [
+      494,
+      460,
+      687,
+      495
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      693,
+      460,
+      770,
+      495
+     ],
+     "page": 9
+    }
+   ],
+   "title": "authorize",
+   "height": 748
+  },
+  {
+   "image": "workspace-manager.png",
+   "width": 1329,
+   "links": [
+    {
+     "rect": [
+      748,
+      472,
+      814,
+      498
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      440,
+      472,
+      499,
+      497
+     ],
+     "page": 8
+    }
+   ],
+   "title": "workspace-manager",
+   "height": 748
+  },
+  {
+   "image": "favorite-plugins.png",
+   "width": 1329,
+   "links": [
+    {
+     "rect": [
+      748,
+      472,
+      802,
+      497
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      440,
+      472,
+      499,
+      497
+     ],
+     "page": 8
+    }
+   ],
+   "title": "favorite-plugins",
+   "height": 748
+  },
+  {
+   "image": "finish-1.png",
+   "width": 1329,
+   "links": [{
+    "rect": [
+     748,
+     472,
+     802,
+     497
+    ],
+    "page": 5
+   }],
+   "title": "finish-1",
+   "height": 748
+  },
+  {
+   "image": "finish-2.png",
+   "width": 1329,
+   "links": [{
+    "rect": [
+     748,
+     472,
+     802,
+     497
+    ],
+    "page": 8
+   }],
+   "title": "finish-2",
+   "height": 748
+  },
+  {
+   "image": "sign-in.png",
+   "width": 1329,
+   "links": [
+    {
+     "rect": [
+      748,
+      441,
+      814,
+      467
+     ],
+     "page": 8
+    },
+    {
+     "rect": [
+      440,
+      441,
+      512,
+      467
+     ],
+     "page": 9
+    }
+   ],
+   "title": "sign-in",
+   "height": 748
+  },
+  {
+   "image": "account-menu.png",
+   "width": 1329,
+   "links": [{
+    "rect": [
+     1120,
+     199,
+     1172,
+     216
+    ],
+    "page": 9
+   }],
+   "title": "account-menu",
+   "height": 748
+  },
+  {
+   "image": "signed-in.png",
+   "width": 1329,
+   "links": [{
+    "rect": [
+     1115,
+     152,
+     1231,
+     171
+    ],
+    "page": 7
+   }],
+   "title": "signed-in",
+   "height": 748
+  },
+  {
+   "image": "signed-out.png",
+   "width": 1329,
+   "links": [{
+    "rect": [
+     1144,
+     152,
+     1228,
+     171
+    ],
+    "page": 6
+   }],
+   "title": "signed-out",
+   "height": 748
+  }
+ ],
+ "title": "Eclipse-first-start",
+ "highlightLinks": true
+}
